b986c04e01f423b21cb835e711cca46db42655f5
Daniel Hobson <hobsondaniel1994@gmail.com>
2014-06-01 18:58:16 +0100

    files

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+
+
+
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+
+        <div id="div-container">
+            <div id="top-container">
+                <div id="logo-container" class="float-left">
+                    <!-- Logo will be in this container. -->
+                </div>
+
+                <div id="winnings-container" class="float-left white-font-style" >
+                    Winnings container
+                </div>
+
+                <div id="twofourhlottery-container" class="float-left white-font-style" >
+                    24h Lottery
+                </div>
+
+                <div id="balance-container" class="float-left white-font-style" >
+                    Balance and Login?
+                </div>
+
+                <div class="clear"></div>
+
+            </div>
+            <div id="mid-container">
+                <div id="sidemenu" class="float-left">
+                    <ul class="menu">
+                        <li class="menu-item"><a href="#home">Home</a></li>
+                        <li class="menu-item"><a href="#news">News</a></li>
+                        <li class="menu-item"><a href="#contact">Contact</a></li>
+                        <li class="menu-item"><a href="#about">About</a></li>
+                    </ul>
+                </div>
+
+                <div id="main-container" class="float-left">
+                    
+                    <h2 class="white-font-style">Welcome to TriBet!</h2>
+                    <p class="white-font-style">We are a new Eve Online Gambling Service. We hope you have a great time on our site.</p>
+                </div>
+
+                <div class="clear"></div>
+            </div>
+        </div>
+    </body>
+</html>
